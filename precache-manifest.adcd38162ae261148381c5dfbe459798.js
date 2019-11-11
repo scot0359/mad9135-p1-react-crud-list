b67643ede9bd6873ce15d61fc8811ae0a71dee25
Conor Scott <scot0359@algonquinlive.com>
2019-11-10 19:06:16 -0500

    Updates

---
+++ precache-manifest.adcd38162ae261148381c5dfbe459798.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e45b97e299d983062a48c5b275a701",
+    "url": "/mad9135-p1-react-crud-list/index.html"
+  },
+  {
+    "revision": "34591c64a89cc8e851a0",
+    "url": "/mad9135-p1-react-crud-list/static/css/main.3815969e.chunk.css"
+  },
+  {
+    "revision": "9cce64d0fbd95f056463",
+    "url": "/mad9135-p1-react-crud-list/static/js/2.d0fef115.chunk.js"
+  },
+  {
+    "revision": "34591c64a89cc8e851a0",
+    "url": "/mad9135-p1-react-crud-list/static/js/main.819673a8.chunk.js"
+  },
+  {
+    "revision": "93c9bd13cc9b6552c3ad",
+    "url": "/mad9135-p1-react-crud-list/static/js/runtime-main.c813c03e.js"
+  }
+]);
